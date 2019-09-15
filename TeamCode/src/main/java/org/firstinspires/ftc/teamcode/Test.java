a6e8e90561e7b529248a55d6d7a84c54a46d23ab
epicDISZ <itay220204@gmail.com>
2019-09-15 23:15:39 +0300

    Add stuffs, light blinky blinky.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,5 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Test {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.openftc.revextensions2.ExpansionHubEx;
+
+
+@TeleOp(name="yay")
+public class Test extends LinearOpMode {
+
+    ExpansionHubEx expansionHub1;
+    int r,g,b;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        expansionHub1 = hardwareMap.get(ExpansionHubEx.class, "Expansion Hub 1");
+        waitForStart();
+        resetStartTime();
+        expansionHub1.getMotorCurrentDraw(null, 1);
+        r = 0; g = 0; b = 0;
+        for (r = 0; r <= 255; r++) {
+            for (g = 0; g <= 255; g++) {
+                for (b = 0; b <= 255; b++) {
+                    expansionHub1.setLedColor(r, g, b);
+                    if (isStopRequested())
+                        return;
+                }
+            }
+        }
+    }
 }
