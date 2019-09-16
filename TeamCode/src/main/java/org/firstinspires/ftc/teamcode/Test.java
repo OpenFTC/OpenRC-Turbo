ce74b01433398aa0af76eb5fe4b0f384f3366528
Yuval Steuer <yujjek@gmail.com>
2019-09-16 20:57:24 +0300

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevTouchSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -12,34 +13,38 @@ import org.openftc.revextensions2.RevBulkData;
 @TeleOp(name="yay")
 public class Test extends LinearOpMode {
 
+    RevTouchSensor magnet;
     ExpansionHubEx expansionHub1;
     RevBulkData bulkData;
     Telemetry.Item totalCur;
+    Telemetry.Item total;
     int r,g,b;
 
     @Override
     public void runOpMode() throws InterruptedException {
+        magnet = hardwareMap.get(RevTouchSensor.class, "gh");
         expansionHub1 = hardwareMap.get(ExpansionHubEx.class, "Expansion Hub 1");
         waitForStart();
         resetStartTime();
         expansionHub1.getMotorCurrentDraw(null, 1);
-        bulkData = expansionHub1.getBulkInputData();
         r = 0;
         g = 0;
         b = 0;
-        totalCur = telemetry.addData("Total Cur", expansionHub1.getTotalModuleCurrentDraw(ExpansionHubEx.CurrentDrawUnits.MILLIAMPS));
+        totalCur = telemetry.addData("is", magnet.isPressed());
+        total = telemetry.addData("val" , magnet.getValue());
         while (!isStopRequested()) {
-            for (r = 0; r <= 255; r++) {
-                for (g = 0; g <= 255; g++) {
-                    for (b = 0; b <= 255; b++) {
-                        expansionHub1.setLedColor(r, g, b);
-                        totalCur.setValue(expansionHub1.getTotalModuleCurrentDraw(ExpansionHubEx.CurrentDrawUnits.MILLIAMPS));
-                        telemetry.update();
-                        if (isStopRequested())
-                            return;
-                    }
-                }
-            }
+            expansionHub1.setLedColor(0,0,0);
+            while (getRuntime() > 500) {}
+            expansionHub1.setLedColor(0,0,255);
+            while (getRuntime() > 500) {}
+            expansionHub1.setLedColor(0,255,0);
+            while (getRuntime() > 500) {}
+            expansionHub1.setLedColor(255,0,0);
+            while (getRuntime() > 500) {}
+            expansionHub1.setLedColor(255,255,255);
+            totalCur.setValue(magnet.isPressed());
+            total.setValue(magnet.getValue());
+            telemetry.update();
         }
     }
 }
