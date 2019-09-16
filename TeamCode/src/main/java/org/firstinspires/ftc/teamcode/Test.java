a1a43c36cc1aff906f185a8a6de7fa108fe23100
epicDISZ <itay220204@gmail.com>
2019-09-16 15:31:38 +0300

    Add stuffs, light blinky blinky.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -4,13 +4,17 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.openftc.revextensions2.ExpansionHubEx;
+import org.openftc.revextensions2.RevBulkData;
 
 
 @TeleOp(name="yay")
 public class Test extends LinearOpMode {
 
     ExpansionHubEx expansionHub1;
+    RevBulkData bulkData;
+    Telemetry.Item totalCur;
     int r,g,b;
 
     @Override
@@ -19,14 +23,18 @@ public class Test extends LinearOpMode {
         waitForStart();
         resetStartTime();
         expansionHub1.getMotorCurrentDraw(null, 1);
+        bulkData = expansionHub1.getBulkInputData();
         r = 0;
         g = 0;
         b = 0;
+        totalCur = telemetry.addData("Total Cur", expansionHub1.getTotalModuleCurrentDraw(ExpansionHubEx.CurrentDrawUnits.MILLIAMPS));
         while (!isStopRequested()) {
             for (r = 0; r <= 255; r++) {
                 for (g = 0; g <= 255; g++) {
                     for (b = 0; b <= 255; b++) {
                         expansionHub1.setLedColor(r, g, b);
+                        totalCur.setValue(expansionHub1.getTotalModuleCurrentDraw(ExpansionHubEx.CurrentDrawUnits.MILLIAMPS);
+                        telemetry.update();
                         if (isStopRequested())
                             return;
                     }
