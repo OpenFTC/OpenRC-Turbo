a75bf75bd3d076738d01c666f78e4f1fc6d37857
Yuval Steuer <yujjek@gmail.com>
2019-09-17 18:24:17 +0300

    Add Configs.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -2,12 +2,37 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp(name="Drive", group="Drive")
 public class Drive extends OpMode {
+    //Variables
+    double driveSpeed;
+
+    //Robot Hardware
+    private DcMotor rightMotor;
+    private DcMotor forRight;
+    private DcMotor leftMotor;
+    private DcMotor forLeft;
+    private DcMotor intakeMotor;
+    private DcMotor microPolMotor;
+
+    //Telemetry
+    Telemetry.Item teleSpeed;
+
 
     @Override
     public void init() {
+        driveSpeed = 0.8;
+        rightMotor = hardwareMap.get(DcMotor.class, "RightMotor");
+        forRight = hardwareMap.get(DcMotor.class, "ForRight");
+        leftMotor = hardwareMap.get(DcMotor.class, "LeftMotor");
+        forLeft = hardwareMap.get(DcMotor.class, "ForLeft");
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+        microPolMotor = hardwareMap.get(DcMotor.class, "microPolMotor");
+        teleSpeed = telemetry.addData("Drive Speed", driveSpeed);
 
     }
 
