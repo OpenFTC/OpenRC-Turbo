c02efcae9d2e2c2fc11236d647c2d1b59df61393
Yuval Steuer <yujjek@gmail.com>
2019-09-17 18:03:43 +0300

    DriveForGlobal

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Drive", group="Drive")
+public class Drive extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
