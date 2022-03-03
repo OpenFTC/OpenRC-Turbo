64e0ff32b393bdde997d3e6d4571539fe0a553c8
Jaysen Quan <jquan.jaysen3@gmail.com>
2022-03-02 19:53:52 -0500

    fixed interface name

--- Hardware/src/main/java/com/qualcomm/hardware/motors/UnderWaterThruster.java
+++ Hardware/src/main/java/com/qualcomm/hardware/motors/UnderWaterThruster.java
@@ -40,9 +40,9 @@ import com.qualcomm.robotcore.hardware.configuration.annotations.MotorType;
 import org.firstinspires.ftc.robotcore.external.navigation.Rotation;
 
 @MotorType(ticksPerRev=1478.4, gearing=52.8, maxRPM=190, orientation= Rotation.CW)
-@DeviceProperties(xmlTag = "Matrix12vMotor", name = "Matrix 12v Motor", builtIn = true)
+@DeviceProperties(xmlTag = "UnderWaterThruster", name = "Under Water Thruster", builtIn = true)
 @ModernRoboticsMotorControllerParams(P=192, I=44, D=136, ratio=16)
-@DistributorInfo(distributor="Modern Robotics", model="50-0120", url="http://www.modernroboticsinc.com/12v-6mm-motor-kit-2")
-public interface Matrix12vMotor
+@DistributorInfo(distributor="Aqua Robotics", model="UNDERWATER-217", url="http://www.modernroboticsinc.com/12v-6mm-motor-kit-2")
+public interface UnderWaterThruster
     {
     }
